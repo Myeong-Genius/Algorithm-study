0f89d71ece763e83a06f7145ef3b41ee2bea2d21
jayden000106 <cars0106@icloud.com>
2022-08-05 22:23:33 +0900

    wip

--- BOJ/10844_지혁.cpp
+++ BOJ/10844_지혁.cpp
@@ -20,20 +20,20 @@ int main() {
     for(int i = 2; i <= N; i++) {
         for(int j = 0; j < 10; j++) {
             if(j == 0) {
-                dp[i][0] = dp[i - 1][j + 1];
+                dp[i][0] = dp[i - 1][j + 1] % 1000000000;
             }
             else if(j == 9) {
-                dp[i][9] = dp[i - 1][j - 1];
+                dp[i][9] = dp[i - 1][j - 1] % 1000000000;
             }
             else {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1] % 1000000000;
             }
         }
     }
 
-    int result;
-    for(int i = 0; i < 10; i++) {
-        result = result + dp[N][i]
+    int result = 0;
+    for(int i = 0; i <= 9; i++) {
+        result = (result + dp[N][i]) % 1000000000;
     }
     cout << result << endl;
 
