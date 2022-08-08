f5bfe4db0791077a8edecf8804b1e8f13c80d492
jayden000106 <cars0106@icloud.com>
2022-08-08 22:16:37 +0900

    wip

--- BOJ/15486_지혁.cpp
+++ BOJ/15486_지혁.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N;
-int dp[1500001];
-vector<int> T, P;
+int dp[1500002], T[1500002], P[1500002];
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N;
-    for(int i = 0; i < N; i++) {
-        int t, p;
-        cin >> t >> p;
-        T.push_back(t);
-        P.push_back(p);
+    for(int i = 1; i <= N; i++) {
+        cin >> T[i] >> P[i];
     }
 
-    for(int i = 0; i < N; i++) {
-        cout << T[i] << " " << P[i] << endl;
+    for(int i = 1; i <= N; i++) {
+        if(i + T[i] <= N + 1) {
+            if(dp[i + T[i]] < dp[i] + P[i]) {
+                fill(dp + i + T[i], dp + N + 1, dp[i] + P[i]);
+            }
+        }
+    }
+
+    for(int i = 1; i <= N + 1; i++) {
+        cout << dp[i] << endl;
     }
 
     return 0;
