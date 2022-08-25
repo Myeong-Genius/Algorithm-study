7c01a949445425e5b8c72c9d2db1afafa241e8fd
jayden000106 <cars0106@icloud.com>
2022-08-25 15:20:16 +0900

    feat: solved 11054.cpp

--- BOJ/11054_지혁.cpp
+++ BOJ/11054_지혁.cpp
@@ -2,12 +2,44 @@
 
 using namespace std;
 
-int N;
+int N, ans;
+int arr[1001], dp[1001], rdp[1001];
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N;
 
-    cout << N << endl;
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i <= N; i++) {
+        dp[i] = 1;
+        for(int j = 1; j <= i; j++) {
+            if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+
+    for(int i = N; i >= 1; i--) {
+        rdp[i] = 1;
+        for(int j = N; j >= i; j--) {
+            if(arr[i] > arr[j] && rdp[j] + 1 > rdp[i]) {
+                rdp[i] = rdp[j] + 1;
+            }
+        }
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(ans < dp[i] + rdp[i] - 1) {
+            ans = dp[i] + rdp[i] - 1;
+        }
+    }
+    cout << ans << endl;
     
     return 0;
 }
