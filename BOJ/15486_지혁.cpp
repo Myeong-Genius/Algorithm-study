8ff188799b208c2fcc8ae980cb81a645d05a5d4a
jayden000106 <cars0106@icloud.com>
2022-08-25 15:00:45 +0900

    feat: solved 15486.cpp

--- BOJ/15486_지혁.cpp
+++ BOJ/15486_지혁.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int N;
+int N, currentMax;
 int dp[1500002], T[1500002], P[1500002];
 
 int main() {
@@ -16,17 +16,17 @@ int main() {
         cin >> T[i] >> P[i];
     }
 
-    for(int i = 1; i <= N; i++) {
+    for(int i = 1; i <= N + 1; i++) {
+        currentMax = max(currentMax, dp[i]);
+        dp[i] = currentMax;
         if(i + T[i] <= N + 1) {
             if(dp[i + T[i]] < dp[i] + P[i]) {
-                fill(dp + i + T[i], dp + N + 1, dp[i] + P[i]);
+                dp[i + T[i]] = dp[i] + P[i];
             }
         }
     }
 
-    for(int i = 1; i <= N + 1; i++) {
-        cout << dp[i] << endl;
-    }
+    cout << dp[N + 1] << endl;
 
     return 0;
 }
