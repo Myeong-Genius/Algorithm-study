a8361d9e1dd5e8da16812a455d5541c7b00c4db5
jayden000106 <cars0106@icloud.com>
2022-08-25 22:23:55 +0900

    feat: solved 11279.cpp

--- BOJ/11279_지혁.cpp
+++ BOJ/11279_지혁.cpp
@@ -48,16 +48,20 @@ int pop() {
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N;
 
     for(int i = 0; i < N; i++) {
         cin >> x;
         if(x == 0) {
             if(end_point != 0) {
-                cout << pop() << endl;
+                cout << pop() << "\n";
             }
             else {
-                cout << "0" << endl;
+                cout << "0" << "\n";
             }
         }
         else {
