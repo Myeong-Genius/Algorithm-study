a5f6348447bda234ad92f8505a0ad5f68c1f9ba5
ler0n <lyint.kor@gmail.com>
2022-09-01 07:51:47 +0900

    feat) Add 12865, 17144.cpp

---
+++ BOJ/12865_정준우.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N, K, DP[101][100001];
+vector<pair<int, int> > product_list;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> N >> K;
+    for(int i = 1; i <= N; i++){
+        int weight, value;
+
+        cin >> weight >> value;
+        product_list.push_back({weight, value});
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= K; j++){
+            DP[i+1][j] = DP[i][j];
+            if(product_list[i].first <= j){
+                DP[i+1][j] = max(DP[i+1][j], DP[i][j-product_list[i].first] + product_list[i].second);
+            }
+        }
+    }
+    cout << DP[N][K];
+    return 0;
+}
