1f21500441705f8b21ac5b729a1fb72728da16fe
ler0n <lyint.kor@gmail.com>
2022-09-01 21:40:24 +0900

    fix) Fix 12865.cpp

--- BOJ/12865_정준우.cpp
+++ BOJ/12865_정준우.cpp
@@ -9,7 +9,7 @@ int main(){
     cin.tie(NULL);
     
     cin >> N >> K;
-    for(int i = 1; i <= N; i++){
+    for(int i = 0; i < N; i++){
         int weight, value;
 
         cin >> weight >> value;
@@ -17,9 +17,9 @@ int main(){
     }
     for(int i = 0; i < N; i++){
         for(int j = 0; j <= K; j++){
-            DP[i+1][j] = DP[i][j];
+            DP[i + 1][j] = DP[i][j];
             if(product_list[i].first <= j){
-                DP[i+1][j] = max(DP[i+1][j], DP[i][j-product_list[i].first] + product_list[i].second);
+                DP[i + 1][j] = max(DP[i + 1][j], DP[i][j - product_list[i].first] + product_list[i].second);
             }
         }
     }
