d51b7349114667688e89b5865e69a16af9e54c13
ler0n <lyint.kor@gmail.com>
2022-09-05 16:28:16 +0900

    feat) Add 6588.cpp

---
+++ BOJ/6588_정준우.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int n, a, b;
+bool is_not_prime[1000000];
+void check_prime(){
+    for(int i = 2; i < 1000001; i++){
+        if(is_not_prime[i]) continue;
+        for(int j = 2; (i * j) < 1000001; j++) is_not_prime[i * j] = true;
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    check_prime();
+    while(1){
+        cin >> n;
+        if(n == 0) break;
+        a = b = 0;
+        for(int i = 2; i < 1000000; i++){
+            if(!is_not_prime[i] && !is_not_prime[n - i]){
+                a = i, b = n - i;
+                break;
+            }
+        }
+        if(a == 0 && b == 0) cout << "Goldbach's conjecture is wrong.\n";
+        else cout << n << " = " << a << " + " << b << '\n';
+    }
+    return 0;
+}
