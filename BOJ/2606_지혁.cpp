8e1e0a4819f6121dd9a065920ad220f9eeee50dc
jayden000106 <cars0106@icloud.com>
2022-09-07 00:25:03 +0900

    feat: solved 2606.cpp

--- BOJ/2606_지혁.cpp
+++ BOJ/2606_지혁.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
+
+int C, N, result;
+vector<int> computer[101];
+bool check[101];
+queue<int> q;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
+    cin >> C >> N;
+
+    for(int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        computer[a].push_back(b);
+        computer[b].push_back(a);
+    }
+
+    q.push(1);
+    check[1] = true;
+    while(!q.empty()) {
+        int a = q.front();
+        q.pop();
+        for(int i = 0; i < computer[a].size(); i++) {
+            int b = computer[a][i];
+            if(!check[b]) {
+                q.push(b);
+                check[b] = true;
+            }
+        }
+    }
+
+    for(int i = 0; i <= C; i++) {
+        if(check[i]) {
+            result++;
+        }
+    }
+
+    cout << result - 1 << "\n";
+
     return 0;
 }
