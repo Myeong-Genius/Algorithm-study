97efaa1b82533b74c3051dc01e31aa02920d9b6d
jayden000106 <cars0106@icloud.com>
2022-09-09 11:01:01 +0900

    feat: solved 14938.cpp

--- BOJ/14938_지혁.cpp
+++ BOJ/14938_지혁.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+
+#define INF 99999999
 
 using namespace std;
 
 int n, m, r;
 int item[101];
-int graph[101][101];
-bool visited[101];
-int curCount, maxCount;
+vector<pair<int, int> > graph[101];
+int dist[101];
+int ans = 0;
 
-void dfs(int index, int depth) {
-    if(visited[index]) {
-        return;
-    }
-    if(depth > m) {
-        return;
-    }
-    visited[index] = true;
-    curCount += item[index];
-    for(int i = 1; i <= n; i++) {
-        if(graph[index][i] != 0 && !visited[i]) {
-            dfs(i, depth + graph[index][i]);
+void dijkstra(int start) {
+    priority_queue<pair<int, int> > pq;
+    pq.push(make_pair(0, start));
+    dist[start] = 0;
+
+    while(!pq.empty()) {
+        int cost = -pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+
+        for(int i = 0; i < graph[cur].size(); i++) {
+            int next = graph[cur][i].first;
+            int nextCost = cost + graph[cur][i].second;
+
+            if(nextCost < dist[next]) {
+                dist[next] = nextCost;
+                pq.push(make_pair(-nextCost, next));
+            }
         }
     }
-    return;
 }
 
 int main() {
@@ -31,30 +40,36 @@ int main() {
     cout.tie(NULL);
 
     cin >> n >> m >> r;
-
     for(int i = 1; i <= n; i++) {
         cin >> item[i];
     }
-
     for(int i = 0; i < r; i++) {
-        int x, y, v;
-        cin >> x >> y >> v;
-        graph[x][y] = v;
-        graph[y][x] = v;
+        int a, b, l;
+        cin >> a >> b >> l;
+
+        graph[a].push_back(make_pair(b, l));
+        graph[b].push_back(make_pair(a, l));
     }
 
     for(int i = 1; i <= n; i++) {
-        curCount = 0;
+        int tmp = 0;
         for(int j = 1; j <= n; j++) {
-            visited[j] = false;
+            dist[j] = INF;
         }
-        dfs(i, 0);
-        if(curCount > maxCount) {
-            maxCount = curCount;
+        dijkstra(i);
+
+        for(int j = 1; j <= n; j++) {
+            if(dist[j] <= m) {
+                tmp += item[j];
+            }
+        }
+
+        if(ans < tmp) {
+            ans = tmp;
         }
     }
 
-    cout << maxCount << "\n";
-    
+    cout << "\n" << ans << "\n";
+
     return 0;
 }
