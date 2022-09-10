a64ac35d66efe81a503b04ce1a180f37d50a4e8d
jayden000106 <cars0106@icloud.com>
2022-09-10 16:56:42 +0900

    wip

---
+++ BOJ/1991_지혁.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        string a, b, c;
+        cin >> a >> b >> c;
+    }
+
+    return 0;
+} 
