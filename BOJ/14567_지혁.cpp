cde59478d9850a45b76e9645f536bbd547fd3b0f
jayden000106 <cars0106@icloud.com>
2022-09-15 15:13:34 +0900

    wip

--- BOJ/14567_지혁.cpp
+++ BOJ/14567_지혁.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int N, M;
+int in_degree[1001];
+vector<int> graph[1001];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    cin >> N >> M;
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+    }
 
     return 0;
 }
