112a58d8fb13fdd0ac7ada5624738db27ae30a2d
jayden000106 <cars0106@icloud.com>
2022-09-22 21:09:28 +0900

    wip
    - solving 1992.cpp

--- BOJ/1992_지혁.cpp
+++ BOJ/1992_지혁.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 
 int N;
-int media[65][65];
+int media[64][64];
+
+void compress(int n, int x, int y) {
+    if(n == 1) {
+        cout << media[x][y];
+        return;
+    }
+
+    int first_element = media[x][y];
+    bool is_same = true;
+    for(int i = x; i < x + n; i++) {
+        for(int j = y; j < y + n; j++) {
+            if(media[i][j] != first_element) {
+                is_same = false;
+                break;
+            }
+        }
+    }
+
+    if(is_same) {
+        cout << first_element;
+    }
+    else {
+        cout << "(";
+        compress(n / 2, x, y);
+        compress(n / 2, y, x + n / 2);
+        compress(n / 2, y + n / 2, x);
+        compress(n / 2, y + n / 2, x + n / 2);
+        cout << ")";
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -11,8 +41,16 @@ int main() {
     cout.tie(NULL);
 
     cin >> N;
-    for(int i = 1; i <= N; i++) {
-        
+    for(int i = 0; i < N; i++) {
+        string tmp;
+        cin >> tmp;
+        for(int j = 0; j < N; j++) {
+            media[i][j] = tmp[j] - '0';
+        }
     }
+
+    compress(N, 0, 0);
+    cout << "\n";
+
     return 0;
 }
