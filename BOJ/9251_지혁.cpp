35fa85a9a04f8e7499283ebdf2b4f9bf49fd6119
jayden000106 <cars0106@icloud.com>
2022-09-22 19:45:57 +0900

    feat: solved 9251.cpp

--- BOJ/9251_지혁.cpp
+++ BOJ/9251_지혁.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 string a, b;
+int dp[1001][1001];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -10,6 +12,18 @@ int main() {
     cout.tie(NULL);
 
     cin >> a >> b;
+    for(int i = 1; i <= a.size(); i++) {
+        for(int j = 1; j <= b.size(); j++) {
+            if(a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+    }
+
+    cout << dp[a.size()][b.size()] << "\n";
     
     return 0;
 }
