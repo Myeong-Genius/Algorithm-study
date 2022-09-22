da2ec2ccaae4834b91c3b6a24ca7328c117cea3c
duckray <esirrt0819@gmail.com>
2022-09-22 23:08:07 +0900

    modify : refactor BOJ 1992

--- BOJ/1992_김덕래.cpp
+++ BOJ/1992_김덕래.cpp
@@ -7,7 +7,7 @@ char arr[64][64];
 
 void input();
 bool isCompressible(int r1, int c1, int r2, int c2);
-string compress(int r1, int c1, int r2, int c2);
+void compress(int r1, int c1, int r2, int c2);
 void solve();
 
 int main(){
@@ -24,31 +24,29 @@ void input(){
         }
     }
 }
-bool isCompressible(int r1, int c1, int r2, int c2){
-    bool flag;
-    for(int k = 0; k < 2; k++){
-        flag = true;
-        for(int i = r1; i < r2 && flag; i++){
-            for(int j = c1; j < c2 && flag; j++){
-                if(arr[i][j] != k + '0'){
-                    flag = false;
-                }
+bool isCompressible(int r1, int c1, int width){
+    for(int i = r1; i < r1 + width; i++){
+        for(int j = c1; j < c1 + width; j++){
+            if(arr[i][j] != arr[r1][c1]){
+                return false;
             }
         }
-        if(flag) break;
     }
-    return flag;
+    return true;
 }
-string compress(int r1, int c1, int r2, int c2){
-    string str("");
-    if(isCompressible(r1, c1, r2, c2)){
-        return str + arr[r1][c1];
+void compress(int r1, int c1, int width){
+    if(isCompressible(r1, c1, width)){
+        cout << arr[r1][c1];
     }
     else{
-        return str + "(" + compress(r1, c1, r1 + (r2 - r1) / 2, c1 + (c2 - c1) / 2) + compress(r1, c1 + (c2 - c1) / 2, r1 + (r2 - r1) / 2, c2) 
-        + compress(r1 + (r2 - r1) / 2, c1, r2, c1 + (c2 - c1) / 2) + compress(r1 + (r2 - r1) / 2, c1 + (c2 - c1) / 2, r2, c2) + ")";
+        cout << "(";
+        compress(r1, c1, width / 2);
+        compress(r1, c1 + width / 2,  width / 2);
+        compress(r1 + width / 2, c1, width / 2);
+        compress(r1 + width / 2, c1 + width / 2, width / 2);
+        cout << ")";
     }
 }
 void solve(){
-    cout << compress(0, 0, n, n);
+    compress(0, 0, n);
 }
