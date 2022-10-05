c9f021d8334137fd731cdc2324986046a8a97444
jayden000106 <cars0106@icloud.com>
2022-10-05 21:48:17 +0900

    feat: solved 11758.cpp, solving 9935.cpp

--- BOJ/11758_지혁.cpp
+++ BOJ/11758_지혁.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+int x1, y1, x2, y2, x3, y3;
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    double result = (x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2;
+
+    if(result > 0) {
+        cout << "1" << "\n";
+    }
+    else if(result < 0) {
+        cout << "-1" << "\n";
+    }
+    else {
+        cout << "0" << "\n";
+    }
+
     return 0;
 }
--- BOJ/9935_지혁.cpp
+++ BOJ/9935_지혁.cpp
@@ -3,65 +3,32 @@
 
 using namespace std;
 
-string input, explode;
-
-bool explostion() {
-    int index = 0;
-    bool flag = false, has_same = false;
-
-    if(input.size() > explode.size()) {
-        for(int i = 0; i <= input.size() - explode.size(); i++) {
-            if(input[i] == explode[0]) {
-                for(int j = 1; j < explode.size(); j++) {
-                    if(input[i + j] != explode[j]) {
-                        break;
-                    }
-                    if(j == explode.size() - 1) {
-                        has_same = true;
-                    }
-                }
-                if(has_same) {
-                    flag = true;
-                    if(i == 0) {
-                        input = input.substr(explode.size());
-                    }
-                    else if(i == input.size() - explode.size()) {
-                        input = input.substr(0, input.size() - explode.size());
-                    }
-                    else {
-                        string tmp1 = input.substr(0, i);
-                        string tmp2 = input.substr(i + explode.size());
-                        input = tmp1 + tmp2;
-                    }
-                    has_same = false;
-                }
-            }
-        }
-    }
-    else {
-        if(input == explode) {
-            input = "";
-        }
-    }
-    return flag;
-}
+string input, explosion;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> input >> explode;
+    string tmp = "";
+    cin >> input >> explosion;
 
-    while(true) {
-        bool check = explostion();
-        if(!check) {
-            break;
+    for(int i = 0; i < input.size(); i++) {
+        tmp += input[i];
+        if(tmp.size() > explosion.size()) {
+            if(tmp[tmp.size() - 1] == explosion[explosion.size() - 1]) {
+                if(tmp.substr(tmp.size() - explosion.size()) == explosion) {
+                    tmp = tmp.substr(0, tmp.size() - explosion.size());
+                }
+            }
+        }
+        else if(tmp == explosion) {
+            tmp = "";
         }
     }
 
-    if(input.size() != 0) {
-        cout << input << "\n";
+    if(tmp.size() > 0) {
+        cout << tmp << "\n";
     }
     else {
         cout << "FRULA" << "\n";
