af2516220f9c3373c65c98550234ceb28912b5b9
duckray <esirrt0819@gmail.com>
2022-10-11 23:57:33 +0900

    modify : BOJ 1774(did it🥳)

--- BOJ/1774_김덕래.cpp
+++ BOJ/1774_김덕래.cpp
@@ -2,29 +2,31 @@
 #include<algorithm>
 #include<cmath>
 #include<vector>
-#include<iomanip>
+#include <queue>
 using namespace std;
+
+struct Coordinate{
+    long long x, y;
+    Coordinate(long long x_, long long y_) : x(x_), y(y_) {}
+};
 struct Edge{
     int n1, n2;
-    double l;
-    Edge(int _n1, int _n2, double _l) : n1(_n1), n2(_n2), l(_l) { }
-    bool operator<(Edge &e){
-        return l < e.l;
+    double dist;
+    Edge(int n1_, int n2_, double dist_) : n1(n1_), n2(n2_), dist(dist_) { }
+    friend bool operator<(const Edge& e1, const Edge& e2){
+        return e1.dist < e2.dist;
     }
 };
-struct Coordinate{
-    long long x,y;
-};
-Coordinate god_coord[1001];
-vector<Edge> edge;
-int parent[1001], n_node, n_edge;
+vector<Coordinate> nodes;
+priority_queue<Edge> edges;
+int parent[1001], n, m;
 double answer;
 
 void input();
 void solve();
-void compute_edges();
-bool compare_edge(Edge i, Edge j);
-void kruskal();
+void push_edges();
+void union_edges();
+int getroot(int n);
 
 int main(){
     input();
@@ -33,55 +35,58 @@ int main(){
 }
 
 void input(){
-    cin >> n_node >> n_edge;
-    for(int i = 1; i <= n_node; i++){
-        cin >> god_coord[i].x >> god_coord[i].y;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        long long x, y;
+        cin >> x >> y;
+        nodes.push_back(Coordinate(x, y));
     }
-    for(int i = 0; i < n_edge; i++){
+
+    for(int i = 0; i < m; i++){
         int n1, n2;
         cin >> n1 >> n2;
-        if(n1 < n2)
-            edge.push_back(Edge(n1, n2, 0.));
-        else
-            edge.push_back(Edge(n2, n1, 0.));
-        // answer += sqrt(pow((god_coord[n1].x - god_coord[n2].x), 2) + pow((god_coord[n1].y - god_coord[n2].y), 2));
+        if(n1 > n2){
+            int tmp = n1, n1 = n2, n2 = tmp;
+        }
+        edges.push(Edge(n1, n2, 0.));
     }
 }
 
 void solve(){
-    for(int i = 1; i <= n_node; i++)
+    for(int i = 1; i <= n; i++){
         parent[i] = i;
-    compute_edges();
-    sort(edge.begin(), edge.end(), compare_edge);
-    kruskal();
+    }
+    push_edges();
+    union_edges();
     cout << fixed;
     cout.precision(2);
-    cout << round(answer * 100.) / 100.;
+    cout << answer;
 }
 
-void compute_edges(){
-    for(int i = 2; i <= n_node; i++){
-        for(int j = 1; j < i; j++){
-            long long dx = (god_coord[i].x - god_coord[j].x) * (god_coord[i].x - god_coord[j].x);
-            long long dy = (god_coord[i].y - god_coord[j].y) * (god_coord[i].y - god_coord[j].y);
-            edge.push_back(Edge(j, i, sqrt(dx + dy)));
+void push_edges(){
+    for(int i = 0; i < nodes.size() - 1; i++){
+        for(int j = i + 1; j < nodes.size(); j++){
+            double dist = sqrt(pow((nodes[i].x - nodes[j].x), 2) + pow((nodes[i].y - nodes[j].y), 2));
+            edges.push(Edge(i + 1, j + 1, -dist));
         }
     }
 }
 
-bool compare_edge(Edge i, Edge j){
-    return i.l < j.l;
+void union_edges(){
+    for(int i = 0; i < n - 1;){
+        Edge e = edges.top();
+        int p1 = getroot(e.n1), p2 = getroot(e.n2);
+        if(p1 != p2){
+            parent[p2] = parent[p1]= parent[e.n1] = parent[e.n2] = min(p1, p2);
+            answer -= e.dist;
+            i++;
+        }
+        edges.pop();
+    }
 }
 
-void kruskal(){
-    n_edge = 0;
-    int j = 0;
-    while(n_edge != n_node - 1){
-        Edge e = edge[j++];
-        if(parent[e.n1] != parent[e.n2]){
-            parent[e.n2] = parent[e.n1];
-            answer += e.l;
-            n_edge++;
-        }
-    }
+int getroot(int n){
+    while(parent[n] != n)
+        n = parent[n];
+    return n;
 }
