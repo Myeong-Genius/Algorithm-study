7faca4a045f32abfe088538d25b0023c9b5378ac
jayden000106 <cars0106@icloud.com>
2022-10-13 01:47:31 +0900

    feat: solved 2981.cpp

--- BOJ/1406_지혁.cpp
+++ BOJ/1406_지혁.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string docs;
+int N, cursor, string_front, string_back;
+
+void excute(string command) {
+    if(command[0] == 'L') {
+        if(cursor != 0) {
+            cursor -= 1;
+        }
+    }
+    else if(command[0] == 'D') {
+        if(cursor != docs.size()) {
+            cursor += 1;
+        }
+    }
+    else if(command[0] == 'B') {
+        if(cursor != 0) {
+            docs.erase(cursor, cursor + 1);
+        }
+    }
+    else if(command[0] == 'P') {
+        string insert_char;
+        docs.insert(cursor, insert_char + command[2]);
+        cursor++;
+    }
+    cout << "docs : " << docs << ", cursor : " << cursor << "\n";
+}
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> docs >> N;
+    cin.ignore();
+
+    cursor = docs.size();
+    for(int i = 0; i < N; i++) {
+        string command;
+        getline(cin, command);
+        excute(command);
+    }
+
+    cout << docs << "\n";
+
     return 0;
 }
--- BOJ/2981_지혁.cpp
+++ BOJ/2981_지혁.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+int N;
+vector<int> arr, result;
+
+int gcd(int a, int b) {
+    return a % b ? gcd(b, a % b) : b;
+}
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(), arr.end());
+
+    int m = arr[1] - arr[0];
+    for(int i = 2; i < N; i++) {
+        m = gcd(m, arr[i] - arr[i - 1]);
+    }
+
+    for(int i = 1; i * i <= m; i++) {
+        if(m % i == 0) {
+            result.push_back(i);
+            if(i != m / i) {
+                result.push_back(m / i);
+            }
+        }
+    }
+    sort(result.begin(), result.end());
+
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
+
     return 0;
 }
