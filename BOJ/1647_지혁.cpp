4f0d8c42e66c3b16286561e3689fdd1d6c1b135a
jayden000106 <cars0106@icloud.com>
2022-10-13 20:38:01 +0900

    feat: solved 1647.cpp

--- BOJ/1647_지혁.cpp
+++ BOJ/1647_지혁.cpp
@@ -1,7 +1,72 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+int N, M, ans;
+int parent[100001];
+vector<pair<int, pair<int, int> > > edge;
+vector<int> v;
+
+int find_parent(int x) {
+    if(x == parent[x]) {
+        return x;
+    }
+    else {
+        return parent[x] = find_parent(parent[x]);
+    }
+}
+
+void union_parent(int x, int y, int c) {
+    x = find_parent(x);
+    y = find_parent(y);
+
+    if(x == y) {
+        return;
+    }
+    parent[y] = x;
+    N--;
+}
+
+bool is_same_parent(int x, int y) {
+    x = find_parent(x);
+    y = find_parent(y);
+    if(x == y) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    for(int i = 0; i < M; i++) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        edge.push_back(make_pair(C, make_pair(A, B)));
+    }
+
+    sort(edge.begin(), edge.end());
+    for(int i = 1; i <= N; i++) {
+        parent[i] = i;
+    }
+    for(int i = 0; i < edge.size(); i++) {
+        if(!is_same_parent(edge[i].second.first, edge[i].second.second)){
+            union_parent(edge[i].second.first, edge[i].second.second, edge[i].first);
+            v.push_back(edge[i].first);
+        }
+    }
+
+    for(int i = 0; i < v.size() - 1; i++) {
+        ans = ans + v[i];
+    }
+    cout << ans << "\n";
+
     return 0;
 }
