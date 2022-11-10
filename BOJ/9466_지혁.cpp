fa098db9115009009c46b156c2b815251fd087a5
jayden000106 <cars0106@icloud.com>
2022-11-10 22:19:46 +0900

    wip

--- BOJ/9466_지혁.cpp
+++ BOJ/9466_지혁.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
+int T, n;
+int graph[100001];
+bool visited[100001];
+bool done[100001];
+int cnt;
+
+void has_cycle(int node) {
+    visited[node] = true;
+    int next = graph[node];
+
+    if(!visited[next]) {
+        has_cycle(next);
+    }
+    else if(!done[next]) {
+        for(int i = next; i != node; i = graph[i]) {
+            cnt++;
+        }
+        cnt++;
+    }
+    done[node] = true;
+}
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+        cin >> n;
+        for(int j = 1; j <= n; j++) {
+            cin >> graph[i];
+        }
+        for(int j = 1; j <= n; j++) {
+            if(!visited[i]) {
+                has_cycle(i);
+            }
+        }
+        cout << n - cnt << "\n";
+        cnt = 0;
+        memset(visited, false, sizeof(visited));
+        memset(done, false, sizeof(done));
+    }
+
     return 0;
 }
